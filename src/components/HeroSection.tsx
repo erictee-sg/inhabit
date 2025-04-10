99c24c424367d4e8b3020cf016a02008947e2873
erictee-sg <erictee@gmail.com>
2025-04-10 10:25:49 +0000

    padding logo

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -257,7 +257,7 @@ const HeroSection = ({
         <div className="flex flex-col md:flex-row h-full items-center">
           {/* First column (1/3) - Logo and date/location */}
           <div
-            className={`w-full md:w-1/3 flex flex-col items-center md:items-start md:pr-8 self-start ${isMobile ? "pt-20" : "pt-8"} md:pt-12`}
+            className={`w-full md:w-1/3 flex flex-col items-center md:items-start md:pr-8 self-start ${isMobile ? "pt-40" : "pt-8"} md:pt-12`}
           >
             {/* Logo */}
             <img
